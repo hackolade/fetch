d77943ace5ec2e2f42833778622a314d2ab1031f
Thomas Jakemeyn <thomas.jakemeyn@gmail.com>
2024-10-02 13:28:48 +0200

    Simplified typing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-type FetchParameters = Parameters<typeof Electron.net.fetch> | Parameters<typeof globalThis.fetch>;
+type FetchParameters = Parameters<typeof globalThis.fetch>;
 
-type FetchReturnType<P> =
-  P extends Parameters<typeof Electron.net.fetch>
-    ? ReturnType<typeof Electron.net.fetch>
-    : ReturnType<typeof globalThis.fetch>;
+type FetchReturnType = ReturnType<typeof globalThis.fetch>;
 
 /**
  * @see https://www.electronjs.org/docs/latest/api/process#processtype-readonly
@@ -22,34 +19,28 @@ function isElectronUtilityProcess(): boolean {
 /**
  * @see https://www.electronjs.org/docs/latest/api/net
  */
-function useElectronNet(params: FetchParameters): params is Parameters<typeof Electron.net.fetch> {
+function useElectronNet(): boolean {
   return isElectronMain() || isElectronUtilityProcess();
 }
 
 /**
  * @see https://www.electronjs.org/docs/latest/api/net
  */
-async function fetchUsingElectronNet(
-  params: Parameters<typeof Electron.net.fetch>,
-): ReturnType<typeof Electron.net.fetch> {
+async function fetchUsingElectronNet(params: FetchParameters): FetchReturnType {
   const { net } = await import('electron');
-  return net.fetch(...params);
+  return net.fetch(...(params as Parameters<typeof Electron.net.fetch>));
 }
 
 /**
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
  */
-async function fetchUsingGlobalContext(
-  params: Parameters<typeof globalThis.fetch>,
-): ReturnType<typeof globalThis.fetch> {
+async function fetchUsingGlobalContext(params: FetchParameters): FetchReturnType {
   if (typeof globalThis.fetch !== 'function') {
     throw new Error('fetch() is not available in the global context!');
   }
   return globalThis.fetch(...params);
 }
 
-export function hckFetch<P extends FetchParameters>(...params: P): FetchReturnType<P> {
-  return useElectronNet(params) ? fetchUsingElectronNet(params) : fetchUsingGlobalContext(params);
+export function hckFetch(...params: FetchParameters): FetchReturnType {
+  return useElectronNet() ? fetchUsingElectronNet(params) : fetchUsingGlobalContext(params);
 }
-
-globalThis.hckFetch = hckFetch;
