3962d8d118b751df10de8d96e8aa82254d6daa45
chulanovskyi <oleg.chulanovskyi@binary-studio.com>
2024-11-01 16:09:40 +0200

    Initial implementation of a specific AWS http handler

---
+++ src/awsHttpHandler/httpHandler.ts
@@ -0,0 +1,106 @@
+import { hckFetch } from '../index';
+import { requestTimeout } from './requestTimeout';
+import { isEmpty } from './isEmpty';
+
+export type Logger = {
+  log: (...content: unknown[]) => string;
+};
+
+export type HttpHandlerConstructor = {
+  requestTimeout?: number;
+  logger?: Logger;
+};
+
+type RequestOptions = {
+  headers: Headers;
+  body?: any;
+  method: string;
+  signal?: AbortSignal;
+};
+
+export class HttpHandler {
+  requestTimeout?: HttpHandlerConstructor['requestTimeout'];
+  logger?: Logger;
+
+  constructor({ requestTimeout, logger }: HttpHandlerConstructor = {}) {
+    this.requestTimeout = requestTimeout;
+    this.logger = logger;
+  }
+
+  getAbortError = () => {
+    const abortError = new Error('Request aborted');
+    abortError.name = 'AbortError';
+    return abortError;
+  };
+
+  async handle(request: any, { abortSignal }: { abortSignal?: AbortSignal } = {}) {
+    if (abortSignal?.aborted) {
+      throw this.getAbortError();
+    }
+
+    const { method, query, protocol, port, hostname, body, headers: requestHeaders } = request;
+
+    const headers = new Headers(requestHeaders);
+    const headersToSkip = ['content-length', 'host'];
+    headersToSkip.forEach(header => headers.delete(header));
+
+    let { path } = request;
+
+    if (!isEmpty(query) && typeof query === 'object') {
+      path += `?${new URLSearchParams(query).toString()}`;
+    }
+
+    const portParam = port ? `:${port}` : '';
+
+    const url = `${protocol}//${hostname}${portParam}${path}`;
+
+    const methodsWithoutBody = ['GET', 'HEAD'];
+
+    const requestOptions: RequestOptions = {
+      body: methodsWithoutBody.includes(method) ? undefined : body,
+      headers,
+      method,
+    };
+
+    if (typeof AbortController !== 'undefined') {
+      requestOptions.signal = abortSignal;
+    }
+
+    const fetchRequest = new Request(url, requestOptions);
+
+    this.logger?.log('info', { fetchRequest }, 'Http request details');
+
+    const resultResolver = async (result: any) => {
+      const fetchHeaders = result.headers;
+      const transformedHeaders: Record<string, string> = {};
+
+      for (const [key, value] of fetchHeaders.entries()) {
+        transformedHeaders[key] = value;
+      }
+
+      const hasReadableStream = result.body !== undefined;
+
+      const response = {
+        headers: transformedHeaders,
+        statusCode: result.status,
+        body: hasReadableStream ? result.body : await result.blob(),
+      };
+
+      return { response };
+    };
+
+    const race = [hckFetch(fetchRequest).then(resultResolver), requestTimeout({ requestTimeout: this.requestTimeout })];
+
+    if (abortSignal) {
+      race.push(
+        new Promise((_, reject) => {
+          abortSignal.onabort = () => {
+            reject(this.getAbortError());
+          };
+        }),
+      );
+    }
+
+    return Promise.race(race);
+  }
+}
