399f94c142f9b7aa8c73f05121c2614d6dfe4399
Thomas Jakemeyn <thomas.jakemeyn@gmail.com>
2025-03-20 14:23:17 +0100

    Moved default export to barrel file for making this library a drop-in replacement of node-fetch

--- src/hckFetch.ts
+++ src/hckFetch.ts
@@ -44,9 +44,3 @@ async function fetchUsingGlobalContext(params: FetchParameters): FetchReturnType
 export function hckFetch(...params: FetchParameters): FetchReturnType {
   return useElectronNet() ? fetchUsingElectronNet(params) : fetchUsingGlobalContext(params);
 }
-
-/**
- * The default export aims at making this library a drop-in replacement for node-fetch.
- * @see https://github.com/googleapis/teeny-request/blob/4a5c834451a5649f68301385356677d8b3809054/src/index.ts#L30
- */
-export default hckFetch;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import { hckFetch } from './hckFetch.js';
+
 export * from './hckFetch.js';
 
 export * from './hckFetchAwsSdkHttpHandler.js';
+
+/**
+ * The default export aims at making this library a drop-in replacement for node-fetch.
+ * @see https://github.com/googleapis/teeny-request/blob/4a5c834451a5649f68301385356677d8b3809054/src/index.ts#L30
+ */
+export default hckFetch;
