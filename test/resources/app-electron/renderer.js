731f2ac0946e29a797a03e6f16c7ddc63ac694a3
ugo.bechameil <ugo.bechameil@hackolade.com>
2025-05-13 11:01:29 +0200

    Fix import now that lib dist is bundled

--- test/resources/app-electron/renderer.js
+++ test/resources/app-electron/renderer.js
@@ -1,5 +1,5 @@
 (async function () {
-  const { hckFetch } = await import('../../../dist/esm/hckFetch.js');
+  const { hckFetch } = await import('../../../dist/esm/index.js');
   const { onHckFetchResult, sendHckFetchResult } = window.electronAPI;
 
   function renderHckFetchResult({ process, isSuccess }) {
